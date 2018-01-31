16d9db334a815788887d86f338d6fcfc1d05074a
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-01-30 20:03:48 -0800

    added firebase to project

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA4aD00BLF849GaWCnKG0uVLR7kZ5hiN2I",
+  authDomain: "train-scheduler-6bddd.firebaseapp.com",
+  databaseURL: "https://train-scheduler-6bddd.firebaseio.com",
+  projectId: "train-scheduler-6bddd",
+  storageBucket: "train-scheduler-6bddd.appspot.com",
+  messagingSenderId: "109817609650"
+};
+firebase.initializeApp(config);
+
+$("#addTrainButton").on("click", function(event) {
+	event.preventDefault();
+
+	// Stores input values into variables
+	var trainName = $("#train-name-input").val().trim();
+	var destination = $("#destination-input").val().trim();
+	var firstTrainTime = $("#first-train-input").val().trim();
+	var frequency = $("#frequency-input").val().trim();
+
+	console.log(trainName, destination, firstTrainTime, frequency);
+});
