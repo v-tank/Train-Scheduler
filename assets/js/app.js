db21f81d272551c1c9e6b417aa47251f6aef1656
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-01-31 20:55:36 -0800

    working code without interval timer

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,25 +9,44 @@ var config = {
 };
 firebase.initializeApp(config);
 
+// creates an instance of the databse
 var database = firebase.database();
-console.log(moment());
 
+// fires off when a child is added to the root
 database.ref().on("child_added", function(snapshot) {
-	console.log(snapshot.val());
-	// console.log(snapshot.val().name);
-
+	// console.log(snapshot.val());
+	
+	// captures values returned from the database into variables
 	var retrievedName = snapshot.val().name;
 	var retrievedDestination = snapshot.val().destination;
 	var retrievedTrainTime = snapshot.val().firstTrainTime;
 	var retrievedFrequency = snapshot.val().frequency;
-	var minutesAway = 5;
-	// console.log(retrievedName);
-	var tableRow = $("<tr><td>" + retrievedName + "</td><td>" + retrievedDestination + "</td><td>" + retrievedFrequency + "</td><td>" + retrievedTrainTime + "</td><td>" + minutesAway + "</td></tr>");
 
+	// format the initial time into HH:mm
+	var timeToSubtract = moment(retrievedTrainTime, "HH:mm");
+
+	// subtract the initial time from the current time and return the answer in minutes
+	var difference = moment().diff(timeToSubtract, "minutes");
+
+	// get the remainder of the difference / frequency by using the modulus operator
+	var remainder = difference % retrievedFrequency;
+
+	// calculate the minutes away by taking the frequency minus the remainder
+	var minutesAway = retrievedFrequency - remainder;
+
+	// console.log(minutesAway);
+
+	// calculate the next arrival time by adding the minutes away to the current time and format it as hh:mm A
+	var nextArrival = moment().add(minutesAway, 'minutes').format("hh:mm A");
+
+	// creates a table row and inserts the retrieved data onto the page
+	var tableRow = $("<tr><td>" + retrievedName + "</td><td>" + retrievedDestination + "</td><td>" + retrievedFrequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+
+	// appends the table row to the main page
 	$("#train-table").append(tableRow);
 });
 
-
+// saves form values into variables when the 'Add' button is clicked
 $("#addTrainButton").on("click", function(event) {
 	event.preventDefault();
 
@@ -37,7 +56,7 @@ $("#addTrainButton").on("click", function(event) {
 	var firstTrainTime = $("#first-train-input").val().trim();
 	var frequency = $("#frequency-input").val().trim();
 
-	console.log(trainName, destination, firstTrainTime, frequency);
+	// console.log(trainName, destination, firstTrainTime, frequency);
 
 	// object to push to database
 	var trainObject = {
@@ -47,9 +66,8 @@ $("#addTrainButton").on("click", function(event) {
 		frequency: frequency
 	}
 
+	// pushes the created object onto the database at the root level
 	database.ref().push(trainObject);
-
-	// firebase.database().ref().push(trainObject);
 
 	// Alerts the user once train has been added to the database
 	alert("Train added!");
